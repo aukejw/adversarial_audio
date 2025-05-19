63c5978ffd2d7a15ee724d5cf3df9305ce52bec7
Auke Wiggers <wiggers.auke@gmail.com>
2025-05-19 14:17:38 +0200

    Move functions to own files where possible.

--- mlx_audio_opt/audio/stft.py
+++ mlx_audio_opt/audio/stft.py
@@ -1,6 +1,5 @@
-from typing import Tuple, Union
+from typing import Union
 
-import librosa
 import mlx.core as mx
 import numpy as np
 from mlx_whisper.audio import HOP_LENGTH, N_FFT, N_SAMPLES, hanning, mel_filters, stft
@@ -8,19 +7,19 @@ from mlx_whisper.audio import HOP_LENGTH, N_FFT, N_SAMPLES, hanning, mel_filters
 from mlx_audio_opt.audio.spectrogram import Spectrogram
 
 __all__ = [
-    "get_magnitude_and_phase",
+    "get_spectrogram",
     "reconstruct_audio_from_magnitude_and_phase",
     "magnitudes_to_log_mel_spectrogram",
 ]
 
 
-def get_magnitude_and_phase(
+def get_spectrogram(
     audio_series: Union[mx.array, np.ndarray],
     n_fft: int = N_FFT,
     hop_length: int = HOP_LENGTH,
     pad_audio: int = N_SAMPLES,
     remove_last_frame: bool = True,
-) -> Tuple[np.ndarray, np.ndarray]:
+) -> Spectrogram:
     """Whisper-compliant STFT of the audio signal.
 
     Args:
@@ -30,8 +29,7 @@ def get_magnitude_and_phase(
         sampling_rate: The sampling rate of the audio.
 
     Returns:
-        magnitudes: The magnitudes (power=1), num_frequencies x num_frames.
-        phase: The phase, obtained using np.angle, num_frequences x num_frames.
+        Spectrogram holding magnitudes and phase as (num_freq x num_frames).
 
     """
     if isinstance(audio_series, np.ndarray):
@@ -58,60 +56,7 @@ def get_magnitude_and_phase(
         magnitudes = magnitudes[:, :-1]
         phase = phase[:, :-1]
 
-    return magnitudes, phase
-
-
-def reconstruct_audio_from_magnitude_and_phase(
-    magnitudes: Union[mx.array, np.ndarray],
-    phase: np.ndarray,
-    n_fft: int = N_FFT,
-    hop_length: int = HOP_LENGTH,
-    window: Union[str, np.ndarray] = "hann",
-    length: int = None,
-    **stft_kwargs,
-):
-    """Reconstruct audio from the given magnitudes and phase.
-
-    Parameters
-    ----------
-    magnitudes: array
-        The magnitude of the STFT output. Mx arrays are assumed to be
-        in Whisper format, i.e. (n_frames, n_freqs), and converted.
-    phase: array, shape = (n_frames, M)
-        The phase of the STFT output.
-    n_fft: int
-        The number of FFT points.
-    hop_length: int
-        The number of samples between frames.
-    length: int
-        The length of the output audio. If None, the length is inferred.
-
-    Returns
-    -------
-    np.array, shape = (*)
-        The reconstructed audio waveform.
-
-    """
-    if isinstance(magnitudes, mx.array):
-        assert magnitudes.T.shape == phase.shape, (
-            f"magnitudes.T and phase must have the same shape, "
-            f"but got {magnitudes.T.shape} and {phase.shape} "
-        )
-        magnitudes = Spectrogram.from_whisper(magnitudes).librosa_array
-
-    # librosa expects (M, n_frames) shape
-    complex_spectrogram = magnitudes * np.exp(1j * phase)
-
-    audio = librosa.istft(
-        complex_spectrogram,
-        n_fft=n_fft,
-        hop_length=hop_length,
-        window=window,
-        length=length,
-        center=False,
-        **stft_kwargs,
-    )
-    return audio
+    return Spectrogram(magnitudes, phase)
 
 
 def magnitudes_to_log_mel_spectrogram(
