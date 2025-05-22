8bf2f31f15303dd7cf8cca64d8a186be239cc08a
Auke Wiggers <wiggers.auke@gmail.com>
2025-05-22 11:56:08 +0200

    Simplify spec

--- mlx_audio_opt/audio/spectrogram.py
+++ mlx_audio_opt/audio/spectrogram.py
@@ -1,3 +1,4 @@
+import copy
 from typing import Optional, Self, Union
 
 import mlx.core as mx
@@ -10,18 +11,31 @@ class Spectrogram:
     We canonically represent a spectrogram as a 2d array, with shape
     (n_mel, n_frames), same as librosa.
 
+    Args:
+        magnitudes: Magnitudes as a 2D array (n_bins, n_frames).
+        phase: Phase is a 2D array (n_bins, n_frames), can be None.
+
     """
 
     def __init__(
         self,
         magnitudes: Union[np.ndarray, mx.array],
-        phase: Optional[np.ndarray] = None,
+        phase: Optional[Union[np.ndarray, mx.array]] = None,
     ):
-        self.magnitudes = magnitudes
-        self.phase = phase
+        self.magnitudes = mx.array(magnitudes)
+        self.phase = mx.array(phase)
 
-    def trim(self, n_frames: int) -> Self:
-        """Trim the spectrogram to the specified number of frames."""
+        if phase is not None and magnitudes.shape != phase.shape:
+            raise ValueError(
+                f"magnitudes.shape={magnitudes.shape}), and phase.shape={phase.shape}. "
+                f"These must have the same shape."
+            )
+
+    def trim(
+        self,
+        n_frames: int,
+    ) -> Self:
+        """Return a spectrogram trimmed to the specified number of frames."""
         if n_frames < self.num_frames:
             magnitudes = self.magnitudes[:, :n_frames]
             phase = self.phase[:, :n_frames] if self.phase is not None else None
@@ -32,6 +46,12 @@ class Spectrogram:
         return Spectrogram(magnitudes, phase)
 
     @property
+    def complex_spectrogram(self):
+        if self.phase is None:
+            raise ValueError("Phase information is not available.")
+        return np.array(self.magnitudes * mx.exp(1j * self.phase))
+
+    @property
     def shape(self):
         return self.magnitudes.shape
 
@@ -40,21 +60,9 @@ class Spectrogram:
         return self.magnitudes.shape[1]
 
     @property
-    def num_frequencies(self):
+    def num_bins(self):
         return self.magnitudes.shape[0]
 
     @property
-    def librosa_array(self):
-        return self.magnitudes
-
-    @property
     def whisper_tensor(self):
-        return mx.array(self.magnitudes.T).astype(mx.float32)
-
-    @classmethod
-    def from_librosa(cls, spectrogram: np.ndarray):
-        return cls(spectrogram)
-
-    @classmethod
-    def from_whisper(cls, spectrogram: mx.array):
-        return cls(np.array(spectrogram.T))
+        return copy.deepcopy(self.magnitudes.T).astype(mx.float32)
